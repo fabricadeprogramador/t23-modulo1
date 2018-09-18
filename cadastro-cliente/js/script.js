c369e70fe86b08be4f40c07e546472908328aad4
Vinícius Souza <viniciuspodi@gmail.com>
2018-09-17 21:30:46 -0400

    Criando repositório do módulo 1

---
+++ cadastro-cliente/js/script.js
@@ -0,0 +1,10 @@
+function abrirMenu(){
+    document.getElementById("menu").classList.add("menu-aberto");
+}
+function fecharMenu(){
+    document.getElementById("menu").classList.remove("menu-aberto");
+}
+
+function fecharMensagem(){
+    document.querySelector(".mensagens").classList.remove("show");
+}
---
+++ calculadora/js/script.js
@@ -0,0 +1,40 @@
+function somar() {
+    var n1 = document.getElementById("input1").value;
+
+    var n2 = document.getElementById("input2").value;
+
+    var resultado = parseFloat(n1) + parseFloat(n2);
+
+    alert(resultado);
+}
+
+function subtrair() {
+    var n1 = document.getElementById("input1").value;
+
+    var n2 = document.getElementById("input2").value;
+
+    var resultado = parseFloat(n1) - parseFloat(n2);
+
+    alert(resultado);
+}
+
+function multiplicar() {
+    var n1 = document.getElementById("input1").value;
+
+    var n2 = document.getElementById("input2").value;
+
+    var resultado = parseFloat(n1) * parseFloat(n2);
+
+    alert(resultado);
+}
+
+function dividir() {
+    var n1 = document.getElementById("input1").value;
+
+    var n2 = document.getElementById("input2").value;
+
+    var resultado = parseFloat(n1) / parseFloat(n2);
+
+    alert(resultado);
+
+}
