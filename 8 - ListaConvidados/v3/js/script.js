057b31297de5776255254efe0181d6b901eab7af
Vinícius Souza <viniciuspodi@gmail.com>
2018-10-08 21:23:17 -0400

    Finalizando editar v3

--- 8 - ListaConvidados/v3/js/script.js
+++ 8 - ListaConvidados/v3/js/script.js
@@ -1,4 +1,5 @@
 var contador = 0;
+var idMarcadoParaEdicao = null;
 
 class ListaConvidados {
 
@@ -8,51 +9,62 @@ class ListaConvidados {
             alert("Insira o nome do convidado!")
         } else {
 
-            //Pegando o nome do convidado do input
-            let nomeConvidado = document.getElementById("inputConvidado").value;
 
-            //Pegando o elemento div da lista
-            let elementoLista = document.getElementById("lista");
+            if (idMarcadoParaEdicao == null) {
+                //Pegando o nome do convidado do input
+                let nomeConvidado = document.getElementById("inputConvidado").value;
 
-            //Criar os elementos dinamicamente
-            let divExterna = document.createElement("div");
-            let spanInterno = document.createElement("span");
-            let imgInterna = document.createElement("img");
-            let imgEdit = document.createElement("img");
+                //Pegando o elemento div da lista
+                let elementoLista = document.getElementById("lista");
 
-            //Setando classe CSS e ID da div
-            divExterna.classList.add("item-lista");
-            let idNovo = "item-" + contador;
-            divExterna.setAttribute("id", idNovo);
+                //Criar os elementos dinamicamente
+                let divExterna = document.createElement("div");
+                let spanInterno = document.createElement("span");
+                let imgInterna = document.createElement("img");
+                let imgEdit = document.createElement("img");
 
-            //Setando o onClick do imagem de remoção
-            imgInterna.setAttribute("onclick", "lista.remover('" + idNovo + "')");
+                //Setando classe CSS e ID da div
+                divExterna.classList.add("item-lista");
+                let idNovo = "item-" + contador;
+                divExterna.setAttribute("id", idNovo);
 
-            //Setando o onClick do imagem de edição
-            imgEdit.setAttribute("onclick", "lista.editar('" + idNovo + "')");
+                //Setando o onClick do imagem de remoção
+                imgInterna.setAttribute("onclick", "lista.remover('" + idNovo + "')");
+
+                //Setando o onClick do imagem de edição
+                imgEdit.setAttribute("onclick", "lista.editar('" + idNovo + "')");
 
 
-            //Setar os dados necessários
+                //Setar os dados necessários
 
-            //Setar o nome dentro do span
-            spanInterno.innerText = nomeConvidado;
+                //Setar o nome dentro do span
+                spanInterno.innerText = nomeConvidado;
 
-            //Setar a imagem no elemento img
-            imgInterna.setAttribute("src", "img/delete.svg");
+                //Setar a imagem no elemento img
+                imgInterna.setAttribute("src", "img/delete.svg");
 
-            //Setar a imagem no elemento img de edição
-            imgEdit.setAttribute("src", "img/edit.svg");
+                //Setar a imagem no elemento img de edição
+                imgEdit.setAttribute("src", "img/edit.svg");
 
-            //Adicionar span e img como filhos da div criada
-            divExterna.appendChild(spanInterno);
-            divExterna.appendChild(imgInterna);
-            divExterna.appendChild(imgEdit);
+                //Adicionar span e img como filhos da div criada
+                divExterna.appendChild(spanInterno);
+                divExterna.appendChild(imgInterna);
+                divExterna.appendChild(imgEdit);
 
-            //Adicionar a div criada como filha da lista
-            elementoLista.appendChild(divExterna);
+                //Adicionar a div criada como filha da lista
+                elementoLista.appendChild(divExterna);
 
-            //Incremento do contador
-            contador++;
+                //Incremento do contador
+                contador++;
+            } else {
+                 //Pegando o nome do convidado do input
+                 let nomeConvidado = document.getElementById("inputConvidado").value;
+                 document.getElementById(idMarcadoParaEdicao).childNodes[0].innerText = nomeConvidado;
+
+                 idMarcadoParaEdicao = null;
+            }
+
+
         }
     }
 
@@ -60,11 +72,13 @@ class ListaConvidados {
         document.getElementById(idDaRemocao).remove();
     }
 
-    editar(idDaEdicao){
+    editar(idDaEdicao) {
         let elementoDiv = document.getElementById(idDaEdicao);
         let convidadoEditar = elementoDiv.childNodes[0].textContent;
 
         document.getElementById("inputConvidado").value = convidadoEditar;
+
+        idMarcadoParaEdicao = idDaEdicao;
     }
 }
 
