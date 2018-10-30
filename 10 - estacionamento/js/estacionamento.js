af210ea774598eb32edff6ddd8b2b1cae47d38bc
Vinicius Souza <viniciuspodi@gmail.com>
2018-10-29 22:01:42 -0400

    Finalizado JS do Estacionamento, CSS incompleto

--- 10 - estacionamento/js/estacionamento.js
+++ 10 - estacionamento/js/estacionamento.js
@@ -2,6 +2,8 @@ class Estacionamento {
     constructor() {
         this.carros = [];
         this.carro = {};
+        this.contador = 0;
+        this.idEdicao = null;
     }
 
     lerDados() {
@@ -10,19 +12,34 @@ class Estacionamento {
         let marca = document.getElementById("marca").value;
         let ano = document.getElementById("ano").value;
 
-
+        this.carro = {};
 
         this.carro.nome = nome;
         this.carro.marca = marca;
         this.carro.ano = ano;
 
-
     }
 
     salvar() {
-        this.lerDados();   
-        if (this.validar())
-            this.carros.push(this.carro);
+        this.lerDados();
+        if (this.validar()) {
+            if (this.idEdicao != null) {
+                for (let i = 0; i < this.carros.length; i++) {
+                    if (this.carros[i].id == this.idEdicao) {
+                        this.carros[i].nome = this.carro.nome;
+                        this.carros[i].marca = this.carro.marca;
+                        this.carros[i].ano = this.carro.ano;
+                        this.idEdicao = null;
+                    }
+                }
+            } else {
+                this.carro.id = this.contador;
+                this.carros.push(this.carro);
+                this.contador++;
+            }
+            this.gerarTabela();
+        }
+
         this.limpar();
 
     }
@@ -44,7 +61,7 @@ class Estacionamento {
 
         }
 
-        if(mensagem == ""){
+        if (mensagem == "") {
             return true;
         }
 
@@ -60,22 +77,90 @@ class Estacionamento {
     }
 
     gerarTabela() {
-        
+
 
         let tabela = document.getElementById("tabela");
-        tabela.innerHTML="";
-        for(let i=0;i< this.carros.length;i++){
+        tabela.innerHTML = "";
+        for (let i = 0; i < this.carros.length; i++) {
 
-                let linha = tabela.insertRow();
-                let celulaNome = linha.insertCell(0);
-                let celulaMarca = linha.insertCell(1);
-                let celulaAno = linha.insertCell(2);
+            let linha = tabela.insertRow();
+            let celulaNome = linha.insertCell(0);
+            let celulaMarca = linha.insertCell(1);
+            let celulaAno = linha.insertCell(2);
+            let celulaEditar = linha.insertCell(3);
+            let celulaExcluir = linha.insertCell(4);
 
-                let imagemEditar = document.createElement("img");
-                
+            let imagemEditar = document.createElement("img");
+            let imagemExcluir = document.createElement("img");
+
+            imagemEditar.setAttribute("src", "img/edit.svg");
+            imagemExcluir.setAttribute("src", "img/delete.svg");
+
+            imagemEditar.setAttribute("onclick", `estacionamento.editar(${this.carros[i].id})`);
+            imagemExcluir.setAttribute("onclick", `estacionamento.excluir(${this.carros[i].id})`);
+
+            celulaNome.innerHTML = this.carros[i].nome;
+            celulaMarca.innerHTML = this.carros[i].marca;
+            celulaAno.innerHTML = this.carros[i].ano;
+
+            celulaEditar.appendChild(imagemEditar);
+            celulaExcluir.appendChild(imagemExcluir);
+
         }
 
     }
+
+    editar(id) {
+
+        for (let i = 0; i < this.carros.length; i++) {
+            if (id == this.carros[i].id) {
+                this.idEdicao = id;
+                document.getElementById("nome").value = this.carros[i].nome;
+                document.getElementById("marca").value = this.carros[i].marca;
+                document.getElementById("ano").value = this.carros[i].ano;
+            }
+
+
+        }
+    }
+    excluir(id) {
+        if (window.confirm("Deseja excluir?")) {
+            for (let i = 0; i < this.carros.length; i++) {
+                if (this.carros[i].id == id) {
+                    this.carros.splice(i, 1);
+                    this.gerarTabela();
+                }
+            }
+        }
+    }
+
+    resultado() {
+
+        let carroMaisVelho = this.carros[0];
+        let carroMaisNovo = this.carros[0];
+        let carrosMedia = 0;
+
+        for (let i = 0; i < this.carros.length; i++) {
+            carrosMedia = carrosMedia + this.carros[i].ano;
+            if (carroMaisVelho.ano > this.carros[i].ano) {
+                carroMaisVelho = this.carros[i];
+            }
+            if (carroMaisNovo.ano < this.carros[i].ano){
+                carroMaisNovo = this.carros[i];
+            }
+            
+        }
+        carrosMedia = carrosMedia / this.carros.length;
+
+        let stringCampoResposta = `Carro mais novo: ${carroMaisNovo.nome}, ${carroMaisNovo.ano}\n
+        Carro mais velho: ${carroMaisVelho.nome}, ${carroMaisVelho.ano}\n
+        Média dos carros: ${carrosMedia}`;
+
+
+        document.getElementById("campoResposta").innerText = stringCampoResposta;
+    }
+
+
 }
 
 let estacionamento = new Estacionamento();
